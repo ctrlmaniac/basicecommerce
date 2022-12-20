3dde0f373bcfb5c9e51b9dda57b0c30bd96956d5
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-20 15:41:58 +0100

    aggiunge application runner

---
+++ src/main/java/me/ctrlmaniac/basicecommerce/ApplicationRunner.java
@@ -0,0 +1,15 @@
+package me.ctrlmaniac.basicecommerce;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Application running!");
+
+    }
+
+}
