56dfbdda96f722c0edee347e87f42aaef017bc12
Giorgio Leo <gio.leo96@gmail.com>
2022-12-20 17:15:22 +0100

    Sistemato il model in inglese

---
+++ src/main/java/me/ctrlmaniac/basicecommerce/models/Category.java
@@ -0,0 +1,67 @@
+package me.ctrlmaniac.basicecommerce.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Category {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column
+    private String name;
+
+    @Column(unique=true)
+    private String slug;
+
+    @Column
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(String name, String slug, String description) {
+        this.name = name;
+        this.slug = slug;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    
+}
