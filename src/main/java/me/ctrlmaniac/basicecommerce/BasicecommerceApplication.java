4a1e72bc7935c00f6b82ac6e9342db9bf1f2fda8
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-20 15:12:44 +0100

    init spring boot

---
+++ src/main/java/me/ctrlmaniac/basicecommerce/BasicecommerceApplication.java
@@ -0,0 +1,13 @@
+package me.ctrlmaniac.basicecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ctrlmaniac/basicecommerce/BasicecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ctrlmaniac.basicecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
